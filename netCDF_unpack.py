5ce4fb93eca6bbdb26a3a50d8d8cb8dc40606f94
Josh Miller <joshua.b.miller@leidos.com>
2023-05-26 17:34:03 +0000

    Checkpoint

--- netCDF_unpack.py
+++ netCDF_unpack.py
@@ -7,3 +7,11 @@ r = requests.get(url, allow_redirects=True)
 
 with open ('dl/lightning_data.nc', 'wb') as f:
     f.write(r.content)
+
+data = xr.open_dataset('dl/lightning_data.nc')
+# print(data['event_lon'].values)
+# print(data['group_lat'].values)
+# print(data['event_time_offset'].values)
+# subset = data.sel(event_lon=-102.80444895, event_lat=20.977652, method='nearest')
+# print(subset)
+
